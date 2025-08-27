146cd783452ef0f37fa485c988f4ed81dc73cb03
Zhang <ye.zhang@ambiomnode2.isas.de>
2025-08-27 15:37:48 +0200

    update

--- scripts/PathMR-PathGen/1.inference_seg.sh
+++ scripts/PathMR-PathGen/1.inference_seg.sh
@@ -7,7 +7,7 @@ for json_file in "${json_files[@]}"
 do
     echo "运行命令，使用 --val_json_name=${json_file}"
     HF_HOME=/mnt/eternus/users/Yu/project/iccv2025/hf_home deepspeed --include=localhost:0 --master_port=24997 /mnt/eternus/users/Yu/project/iccv2025/PathVR/pathvr_v3/inference_seg.py \
-      --version="/mnt/eternus/users/Yu/project/iccv2025/PathVR/pathvr_v3/result_ckpts/PathMR-PathGen" \
+      --version="result_ckpts/PathMR-PathGen" \
       --dataset_dir="/mnt/eternus/users/Ye/TCGA/" \
       --dataset="multi_part_reason_seg" \
       --multi_reason_seg_data="reason_seg_val" \
